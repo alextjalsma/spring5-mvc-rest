e4a1b04b99fbcbfbe1030162373f12dad6d3e916
atjalsma@home.nl <atjalsma@home.nl>
2020-02-02 20:37:47 +0100

    Initial commit

---
+++ src/test/java/guru/springfamework/Spring5MvcRestApplicationTests.java
@@ -0,0 +1,14 @@
+package guru.springfamework;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Spring5MvcRestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
